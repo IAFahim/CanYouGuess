1ef10559f6d0b4389aa3fbd79326e8db3f01c0ce
fahimmanowarj5 <fahimmanowarj5@gmail.com>
2022-02-08 02:44:53 +0600

    len

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,8 @@
 const canvas = document.getElementById("canvas");
-let ctx = canvas.getContext("2d");
 class PickPercentage {
-    constructor(ctx) {
-        this._ctx = ctx;
+    constructor(canvas) {
+        this._canvas = canvas;
+        this._ctx = canvas.getContext("2d");
         this.setAutoSize();
         this.update();
     }
@@ -12,15 +12,23 @@ class PickPercentage {
         });
     }
     draw() {
-        this._ctx.fillRect(150, 150, 500, 100);
+        this._ctx.fillRect(this._x, this._y, this._width, this._height);
+    }
+    resizeAlpha_clear(anchor) {
+        this._canvas.width = innerWidth;
+        this._canvas.height = innerHeight;
+        this._ctx.clearRect(0, 0, innerWidth, innerHeight);
+        this._width = 1000;
+        this._height = 100;
+        this._x = innerWidth * anchor - this._width * anchor;
+        this._y = innerHeight * anchor - this._height * anchor;
+        console.log(this._x, this._y);
     }
     update() {
-        canvas.width = innerWidth;
-        canvas.height = innerHeight;
-        this._ctx.clearRect(0, 0, innerWidth, innerHeight);
+        this.resizeAlpha_clear(.5);
         this.draw();
     }
 }
-let pickPercentage = new PickPercentage(ctx);
+let pickPercentage = new PickPercentage(canvas);
 pickPercentage.setAutoSize();
 //# sourceMappingURL=index.js.map
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,11 +1,16 @@
 const canvas = document.getElementById("canvas") as HTMLCanvasElement
-let ctx: CanvasRenderingContext2D = canvas.getContext("2d")
 
 class PickPercentage {
+    _canvas: HTMLCanvasElement
     _ctx: CanvasRenderingContext2D;
+    _x: number;
+    _y: number;
+    _width: number;
+    _height: number;
 
-    constructor(ctx: CanvasRenderingContext2D) {
-        this._ctx = ctx
+    constructor(canvas: HTMLCanvasElement) {
+        this._canvas = canvas
+        this._ctx = canvas.getContext("2d")
         this.setAutoSize()
         this.update()
     }
@@ -17,18 +22,27 @@ class PickPercentage {
     }
 
     draw() {
-        this._ctx.fillRect(150, 150, 500, 100)
+        this._ctx.fillRect(this._x, this._y, this._width, this._height)
+    }
+
+    resizeAlpha_clear(anchor: number) {
+        this._canvas.width = innerWidth
+        this._canvas.height = innerHeight
+        this._ctx.clearRect(0, 0, innerWidth, innerHeight)
+        this._width = 1000
+        this._height = 100
+        this._x = innerWidth * anchor - this._width * anchor;
+        this._y = innerHeight * anchor - this._height * anchor;
+        console.log(this._x, this._y)
     }
 
     update() {
-        canvas.width = innerWidth
-        canvas.height = innerHeight
-        this._ctx.clearRect(0, 0, innerWidth, innerHeight)
+        this.resizeAlpha_clear(0.5)
         this.draw()
     }
 }
 
-let pickPercentage = new PickPercentage(ctx)
+let pickPercentage = new PickPercentage(canvas)
 pickPercentage.setAutoSize()
 
 
